cb618777fdf9ae61497831d590c7ffeeb631622e
Brett McGregor <brett.w.mcgregor@gmail.com>
2018-04-18 05:14:48 +1000

    inital commit

---
+++ 31guess_letters.py
@@ -0,0 +1,62 @@
+"""
+Let’s continue building Hangman. In the game of Hangman, a clue word is given
+by the program that the player has to guess, letter by letter. The player
+guesses one letter at a time until the entire word has been guessed. (In
+the actual game, the player can only guess 6 letters incorrectly before losing).
+
+Let’s say the word the player has to guess is “EVAPORATE”. For this exercise,
+write the logic that asks a player to guess a letter and displays letters in
+the clue word that were guessed correctly. For now, let the player guess an
+infinite number of times until they get the entire word. As a bonus, keep
+track of the letters the player guessed and display a different message if
+the player tries to guess that letter again. Remember to stop the game when
+all the letters have been guessed correctly! Don’t worry about choosing a word
+randomly or keeping track of the number of guesses the player has remaining -
+we will deal with those in a future exercise.
+
+An example interaction can look like this:
+
+>>> Welcome to Hangman!
+_ _ _ _ _ _ _ _ _
+>>> Guess your letter: S
+Incorrect!
+>>> Guess your letter: E
+E _ _ _ _ _ _ _ E
+...
+
+And so on, until the player gets the word.
+
+"""
+
+def check_previous(guess, previous_guesses):
+    if guess in previous_guesses:
+        print("You have already guessed that letter! Try again.")
+    else:
+        previous_guesses.append(guess)
+        return previous_guesses
+
+
+word = "EVAPORATE"
+
+#welcome message and prompt for user to guess a letter
+print("Welcome to Hangman!")
+previous_guesses = []
+while True:
+    guess = input("Guess a letter>  ")
+
+    #keep track of previous letters guessed and report an error if the player guesses previous
+    #letter
+    check_previous(guess, previous_guesses)
+    print(previous_guesses)
+
+#check if letter is in word
+
+
+#if in word then print the letters with the remaining blanks
+
+#if not in word then print feedback to user
+
+
+
+#when all letters guessed print win message
+
